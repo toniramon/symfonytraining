785846a622fd572ace133a8474e261c1b42f000f
Toni Ramon <toni.ramon93@gmail.com>
2021-05-30 11:40:32 +0200

    Added Logger to Products Controller

--- symfony/src/Controller/ProductController.php
+++ symfony/src/Controller/ProductController.php
@@ -2,25 +2,36 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ProductController extends AbstractController
 {
 
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("product", name="product_list")
      */
-    public function list()
+    public function index(Request $request)
     {
+        $title = $request->get('title', 'fallbackName');
+        $this->logger->info("This is a test");
         $response = new JsonResponse();
         $response->setData(
             [
                 'success' => true,
                 'data' => [
                     'id' => 1,
-                    'name' => 'tomeu'
+                    'name' => $title
                 ]
             ]
         );
